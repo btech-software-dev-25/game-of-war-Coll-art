81bb034138670b9c526f47b2415378f4ee869298
Coll-art <collin5050m@Gmail.com>
2025-11-06 14:12:43 -0700

    Add Deck class with card initialization and methods
    Implemented Deck class with card management methods.

--- Deck.cs
+++ Deck.cs
@@ -11,7 +11,111 @@ namespace GameOfWar
         public static string[] Suits =
         {
             "Hearts", "Diamonds", "Clubs", "Spades"
+        };namespace GameOfWar
+{
+    public class Deck
+    {
+        public static string[] RankNames =
+        {
+            "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "Jack", "Queen", "King", "Ace"
         };
+
+        public static string[] Suits =
+        {
+            "Hearts", "Diamonds", "Clubs", "Spades"
+        };
+
+
+        // Create a public int property Count that returns the Count value from the private collection _cards
+        public int collection_cards;
+
+        // Create a private field _cards that is a List<Card>
+        private List<Card> card;
+
+        // Create a public constructor that takes two parameter: a List<card> called cards and a boolean value called isEmptyDeck
+        // If cards is not null and has elements in it, assign it to _cards and be done
+        // If cards is null or empty:
+        //     _cards should be initialized as an empty List<Card>
+        //     InitializeDeck() should be called if and only if isEmptyDeck is false
+        public int Deck(List<Card> cards, bool isEmptyDeck = false)//constructor
+        {
+            if (cards != null && cards.Count > 0)
+            {
+                card = cards;
+            }
+            else
+            {
+                card = new List<Card>();
+                if (!isEmptyDeck)
+                {
+                    InitializeDeck();
+                }
+            }
+
+
+        }  // Card newCard = new Card(suits[i], i);
+           // card.Add(newCard);
+
+
+        // Create a private void method called InitializeDeck() which does the following:
+        // Use RankNames and Suits in nested loops to generate all 52 combinations of rank and suit and add them to _cards
+        string[] suits = ["clubs", "hearts", "spades", "dimonds"];
+        private void InitializeDeck()
+        {
+           // string[] suits = ["clubs", "hearts", "spades", "dimonds"];
+
+
+            for (int i = 0; i <= 12; i++)
+            {
+                for (int j = 0; j < suits.Length; j++)
+                {
+                    Card newCard = new Card(suits[j], i);
+                    card.Add(newCard);
+                }
+            }
+        }
+
+        // Create a public void method called Shuffle() which shuffles (rearranges) the cards in _deck
+        public void Shuffle()//finish
+        {
+            foreach(string suit in suits)
+            {
+                
+            }
+        }
+        
+
+        // Create a public method CardAtIndex which takes an int parameter for the index of a card and
+        // returns the card at the index specified, or throws IndexOutOfRangeException if index is too large or too small
+        public int CardAtIndex()
+        {
+            
+
+            return 
+        }
+
+        // Create a public method PullCardAtIndex which does exactly the same thing as CardAtIndex
+        // with the additional feature that it _removes_ the card from the deck
+
+
+        // Create a public method PullAllCards that returns a list of all of the cards in the deck
+        // and removes them all from the deck, leaving it empty
+
+
+        // Create a public method PushCard that accepts a Card as a parameter and adds it to _deck
+
+
+        // Create a public method PushCards that accepts a List<Card> as a parameter and adds the list to _cards
+        // Be sure to use AddRange and not Add
+
+
+        // Create a public method Deal that accepts an integer representing the number of cards to deal
+        // and then removes that many cards from the deck, returning them as a List<Card>
+        // Be sure to check the size of _deck against the number of cards requested so you don't go out
+        // of bounds
+    }
+}
 
 
         // Create a public int property Count that returns the Count value from the private collection _cards
